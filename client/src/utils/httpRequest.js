0cfdf597e36ae89ebb218fccb12f645bf77d41c3
“Your <you@example.com>
2023-11-30 13:28:29 +0700

    Update

--- client/src/utils/httpRequest.js
+++ client/src/utils/httpRequest.js
@@ -4,7 +4,7 @@ const httpRequest = axios.create({
     baseURL:
         process.env.NODE_ENV !== 'production'
             ? process.env.REACT_APP_BASE_URL
-            : 'https://mern-learnit-t3zg.onrender.com/api',
+            : 'https://mern-learnit-t3zg.onrender.com/api/',
 });
 
 export const get = async (path) => {
